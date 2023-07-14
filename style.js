4a7918ea8950e50d5cf3e0ba005af9c3d21e43b4
Mohd <mazim8637@gmail.com>
2023-07-14 13:04:33 +0530

    Initial commit

---
+++ style.js
@@ -0,0 +1,7 @@
+// window.onload = function() {
+//     $('.carousel').carousel({
+//       interval: 1000, // Slide every 5 seconds
+//       pause: 'hover' // Pause on hover
+//     });
+//   };
+  
